d2f70da85139484acdf059643f8b8ec84e95d4b7
Peter Lermann <peter@lermann.name>
2016-05-17 23:46:34 +0200

    commit? more like submit

--- main.py
+++ main.py
@@ -1,19 +1,4 @@
-#!/usr/bin/env python
-#
-# Copyright 2007 Google Inc.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-#
+
 from google.appengine.api import urlfetch
 import webapp2
 import json
@@ -34,6 +19,7 @@ class SetWebHookHandler(webapp2.RequestHandler):
 class MainHandler(webapp2.RequestHandler):
     def get(self):
         self.response.write('Hello worlddeggfkhfkhrp!')
+
 
 
 class WebHookHandler(webapp2.RequestHandler):
@@ -83,9 +69,6 @@ class WebHookHandler(webapp2.RequestHandler):
         elif 'who are you' in text:
             reply('CHNTY IS MY MASTER')
         else:
-            if getEnabled(chat_id):
-                reply('I got your messsage')
-            else:
                 logging.info('not enabled for chat_id {}'.format(chat_id))
 
 app = webapp2.WSGIApplication([
