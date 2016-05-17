3fc3bb7687ec2cfdb7e13ca4d36d42a363c7abc6
Peter Lermann <peter@lermann.name>
2016-05-17 23:32:15 +0200

    various changes

--- main.py
+++ main.py
@@ -16,6 +16,8 @@
 #
 from google.appengine.api import urlfetch
 import webapp2
+import json
+import logging
 from urllib2 import urlopen
 from urllib import urlencode
 
@@ -33,6 +35,62 @@ class MainHandler(webapp2.RequestHandler):
     def get(self):
         self.response.write('Hello worlddeggfkhfkhrp!')
 
+
+class WebHookHandler(webapp2.RequestHandler):
+    def post(self):
+        urlfetch.set_default_fetch_deadline(60)
+        body = json.loads(self.request.body)
+        logging.info('request body:')
+        logging.info(body)
+        self.response.write(json.dumps(body))
+
+        update_id = body['update_id']
+        message = body['message']
+        message_id = message.get('message_id')
+        date = message.get('date')
+        text = message.get('text')
+        fr = message.get('from')
+        chat = message['chat']
+        chat_id = chat['id']
+
+        if not text:
+            logging.info('no text')
+            return
+
+        def reply(msg=None):
+            if msg:
+                resp = urlopen(BASE_URL + 'sendMessage', urlencode({
+                    'chat_id': str(chat_id),
+                    'text': msg.encode('utf-8'),
+                    'parse_mode': 'HTML',
+                    'disable_web_page_preview': 'true',
+                })).read()
+            else:
+                logging.error('no msg specified')
+                resp = None
+
+            logging.info('send response:')
+            logging.info(resp)
+
+        if text.startswith('/'):
+            if text == '/start':
+                reply('Bot enabled')
+            if text == '/help':
+                reply('nope')
+            else:
+                reply('What command?')
+
+        elif 'who are you' in text:
+            reply('CHNTY IS MY MASTER')
+        else:
+            if getEnabled(chat_id):
+                reply('I got your messsage')
+            else:
+                logging.info('not enabled for chat_id {}'.format(chat_id))
+
 app = webapp2.WSGIApplication([
     ('/', MainHandler)
+    ('/set_webhook', SetWebHookHandler),
+    ('/webhook', WebHookHandler),
 ], debug=True)
+
