85435ce1e0f2d79de7e3e4048d5014e37c22583d
Peter Lermann <peter@lermann.name>
2016-05-18 19:38:39 +0200

    to be on the same page

--- main.py
+++ main.py
@@ -5,9 +5,44 @@ import json
 import logging
 from urllib2 import urlopen
 from urllib import urlencode
+import random
+from datetime import datetime
+from google.appengine.ext import ndb
 
 TOKEN = open('bot.token').read()
 BASE_URL = 'https://api.telegram.org/bot' + TOKEN + '/'
+masterid = 90759405
+
+#Start of ndb classes
+
+class Phrase(ndb.Model):
+    phrase = ndb.StringProperty()
+    count = ndb.IntegerProperty()
+    date = ndb.DateTimeProperty()
+    firstPoster = ndb.IntegerProperty()
+
+#End of ndb classes
+
+#Start of Methods
+
+def addPhrase(phrase):
+    Phrase(phrase=phrase, count=1, date=datetime.now(), firstPoster=0).put()
+
+def choosePhrase():
+    allphrase = Phrase.query().fetch()
+    answer = ""
+    leng = 1
+    for phrase in allphrase:
+        if random.randint(0, phrase.count*leng) == 1:
+            answer += " " + phrase.phrase
+            leng += 1
+            phrase.count+=1
+            phrase.put()
+    return answer
+
+#End of Methods
+
+#Start of Handlers
 
 class SetWebHookHandler(webapp2.RequestHandler):
     def get(self):
@@ -16,9 +51,10 @@ class SetWebHookHandler(webapp2.RequestHandler):
         if url:
             self.response.write(json.dumps(json.load(urlopen(BASE_URL + 'setWebhook', urlencode({'url': url})))))
 
+#this is irrelevant for telegrambot but nice to check google appengine
 class MainHandler(webapp2.RequestHandler):
     def get(self):
-        self.response.write('Hello worlddeggfkhfkhrp!')
+        self.response.write('Hello world!')
 
 
 
@@ -35,7 +71,11 @@ class WebHookHandler(webapp2.RequestHandler):
         message_id = message.get('message_id')
         date = message.get('date')
         text = message.get('text')
+        textE=""
+        if(text != None):
+            textE = text.encode('ascii','ignore')
         fr = message.get('from')
+        userid = fr['id']
         chat = message['chat']
         chat_id = chat['id']
 
@@ -59,20 +99,30 @@ class WebHookHandler(webapp2.RequestHandler):
             logging.info(resp)
 
         if text.startswith('/'):
+            if text.find('@') > 0:
+                text = text[0:text.find('@')]
             if text == '/start':
                 reply('Bot enabled')
-            if text == '/help':
+            elif text == '/help':
                 reply('nope')
+            elif text == '/tell':
+                reply(choosePhrase())
             else:
                 reply('What command?')
 
         elif 'who are you' in text:
-            reply('CHNTY IS MY MASTER')
+            reply('I am Chief-Bot of the Bots of Darkness, Lord of the Thirteen Chatrooms, Master of Spam, Emporer of the Groups, Lord of the Unidle, Lord of the Dance(Self-nominated), and the Mayor of a little village down the coast')
+        elif 'derp' in text:
+            reply("derpiderp")
+        elif userid == masterid:
+            reply("I am here to serve, oh my great Master!")
         else:
-                logging.info('not enabled for chat_id {}'.format(chat_id))
+            #reply("I will remember")
+            addPhrase(textE)
+            logging.info('not enabled for chat_id {}'.format(chat_id))
 
 app = webapp2.WSGIApplication([
-    ('/', MainHandler)
+    ('/', MainHandler),
     ('/set_webhook', SetWebHookHandler),
     ('/webhook', WebHookHandler),
 ], debug=True)
